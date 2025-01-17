0460c1e69263e27617cf4c4efa62e4d5fc76b0ec
Matias Pierretti <sreaver18@gmail.com>
2025-01-17 19:48:45 +0100

    feat: create header component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,8 @@
+<template>
+  <header class="h-[var(--header-height)] flex items-center px-10">
+      <img
+        src="../assets/logo.svg"
+        class="h-8"
+      />
+  </header>
+</template>
